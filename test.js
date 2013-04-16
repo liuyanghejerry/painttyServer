c1678223efa75a21352a74881200bc6f18cdef6c
liuyanghejerry <liuyanghejerry@126.com>
2013-04-16 13:23:22 +0800

    add: test.js for simple unit testing.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ test.js
@@ -0,0 +1,9 @@
+#!/usr/local/bin/node
+
+require.paths.push(__dirname);
+// require.paths.push(__dirname + '/deps');
+// require.paths.push(__dirname + '/lib');
+var testrunner = require('nodeunit').testrunner;
+
+process.chdir(__dirname);
+testrunner.run(['tests']);
