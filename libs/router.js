e383e501e7ea1ee68bd720fcab6e86771d43e6ba
liuyanghejerry <liuyanghejerry@126.com>
2014-02-15 01:43:13 +0800

    使用原生的call替代_.bind
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- libs/router.js
+++ libs/router.js
@@ -2,68 +2,66 @@ var util = require("util");
 var _ = require('underscore');
 
 function Router(options) {
-    var defaultOptions = new function () {
-        var self = this;
-        self.name = '';
-        self.log = false;
-    };
-    
-    if (_.isUndefined(options)) {
-        var options = {};
-    }
-    var op = _.defaults(options, defaultOptions);
+  var defaultOptions = new function () {
+    var self = this;
+    self.name = '';
+    self.log = false;
+  };
 
-    this.table = [];
+  if (_.isUndefined(options)) {
+    var options = {};
+  }
+  var op = _.defaults(options, defaultOptions);
+
+  this.table = [];
 }
 
 Router.prototype.reg = function(rule, request, handler, enviroment) {
-    var self = this;
-    if ( !_.isString(rule) || !_.isString(request) || !_.isFunction(handler) ) {
-        return;
-    };
+  var self = this;
+  if ( !_.isString(rule) || !_.isString(request) || !_.isFunction(handler) ) {
+    return;
+  }
 
-    var newRule = {
-        'rule': rule, 
-        'request': request, 
-        'handler': handler,
-        'enviroment': enviroment
-    };
-    self.table.push(newRule);
-    return self;
+  var newRule = {
+    'rule': rule, 
+    'request': request, 
+    'handler': handler,
+    'enviroment': enviroment
+  };
+  self.table.push(newRule);
+  return self;
 }
 
 Router.prototype.message = function(client, msg) {
-    var self = this;
-    if( !_.isObject(msg) && !_.isEmpty(msg) ) {
-        return;
-    }
+  var self = this;
+  if( !_.isObject(msg) && !_.isEmpty(msg) ) {
+    return;
+  }
 
-    if (self.table.length < 1) {
-        return;
-    };
+  if (self.table.length < 1) {
+    return;
+  };
 
-    var rules = _.pluck(self.table, 'rule');
-    _.each(rules, function(ele, index, list) {
-        if(_.has(msg, ele)) {
-            var request = msg[ele];
-            if (self.table[index]['request'] == request) {
-                if (self.env) {
-                    _.bind(self.table[index]['handler'], 
-                        self.table[index]['enviroment'], 
-                        client, 
-                        msg)();
-                }else{
-                    self.table[index]['handler'](client, msg);
-                }
-            };
+  var rules = _.pluck(self.table, 'rule');
+  _.each(rules, function(ele, index, list) {
+    if(_.has(msg, ele)) {
+      var request = msg[ele];
+      var entry = self.table[index];
+      if (entry['request'] == request) {
+        if (entry['enviroment']) {
+          entry['handler'].call(entry['enviroment'], client, msg);
+        }else{
+          entry['handler'](client, msg);
         }
-    });
-    return self;
+      }
+    }
+  });
+  return self;
 }
 
 Router.prototype.clear = function() {
-    this.table = [];
-    return this;
+  this.table = [];
+  return this;
 }
 
 module.exports = Router;
