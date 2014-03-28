259aab8937a2ff3bfc3106dbbf9efd2903b95f84
liuyanghejerry <liuyanghejerry@126.com>
2014-03-28 14:49:53 +0800

    去除localsocket

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var toobusy = require('toobusy');
 var async = require('async');
 var mongoose = require('mongoose');
 var common = require('./libs/common.js');
-var Router = require("./libs/router.js");
 var RoomManager = require('./roommanager.js');
 var socket = require('./libs/streamedsocket.js');
 var CpuCare = require('./libs/cpucare.js');
@@ -17,8 +16,6 @@ var globalConf = common.globalConf;
 // var agent = require('webkit-devtools-agent');
 
 if (cluster.isMaster) {
-  var localSocket = null;
-  var router = null;
   process.title = 'painttyServer master';
   async.auto({
     'init_db': function(callback) {
@@ -32,48 +29,7 @@ if (cluster.isMaster) {
         callback();
       });
     },
-    'init_local_socket': ['init_db', function(callback){
-      router = new Router();
-
-      router.reg('request', 'broadcast', function(cli, obj){
-        var msg = obj['msg'];
-        if (!TypeChecker.isString(msg)) {
-          var ret = {
-            'response': 'broadcast',
-            'result': false
-          };
-          return;
-        };
-        var send_to_workers = {
-          message: 'broadcast',
-          content: msg
-        };
-        var w = cluster.workers;
-        if (w[0]) {
-          w[0].send(send_to_workers);
-        };
-        
-        var ret = {
-          'response': 'broadcast',
-          'result': true
-        };
-        var jsString = common.jsonToString(ret);
-        cli.sendData(cli, new Buffer(jsString));
-      });
-
-      localSocket = new socket.SocketServer({'record': false, 'keepAlive': false});
-
-      localSocket.on('message', function(client, data) {
-        var obj = common.stringToJson(data);
-        logger.log(obj);
-        self.router.message(client, obj);
-      }).on('error', function(err){
-        logger.error(err);
-      });
-      localSocket.listen('56565', '127.0.0.1');
-      callback();
-    }],
-    'fork_child': ['init_local_socket', function(callback){
+    'fork_child': ['init_db', function(callback){
       // Fork workers.
       function forkWorker(memberId) {
         var worker = cluster.fork({'memberId': memberId});
