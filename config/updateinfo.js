dcf888ce96b84880b8a2d4513aa4df8974af9e6d
liuyanghejerry <liuyanghejerry@126.com>
2014-11-08 15:37:53 +0800

    更新版本
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- config/updateinfo.js
+++ config/updateinfo.js
@@ -1,6 +1,6 @@
 var ConfigObject = {
   pubPort: 17979,
-  version: 52,
+  version: 53,
   // TODO: use a text file for changelog
   changelog: {
     'en': './changelog/en.changelog',
@@ -11,9 +11,9 @@ var ConfigObject = {
   defaultlang: 'en',
   level: 2,
   url: {
-    'windows x86': 'http://download.mrspaint.com/0.5/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_x86_0.52.zip',
-    'windows x64': 'http://download.mrspaint.com/0.5/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_x64_0.52.zip',
-    'mac': 'http://download.mrspaint.com/0.5/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_Mac_0.52.zip'
+    'windows x86': 'http://download.mrspaint.com/0.5/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_x86_0.53.zip',
+    'windows x64': 'http://download.mrspaint.com/0.5/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_x64_0.53.zip',
+    'mac': 'http://download.mrspaint.com/0.5/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_Mac_0.53.zip'
   },
   updater: {
     version: 20
